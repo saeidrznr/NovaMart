a520e8bca37413d9f3edad21b99fd02088fa5df9
𝒮𝒶ℯ𝒾𝒹 <saeed.rezaee.nor@gmail.com>
2026-08-14 14:11:25 +0330

    feat: add user model and table

--- database/database.py
+++ database/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-from core.core import settings
+from core.config import settings
 
 engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = DeclarativeBase()
+Base = declarative_base()

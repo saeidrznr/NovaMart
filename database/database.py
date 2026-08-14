a8449a77e54e21afe7cc144b52e8096159856acc
𝒮𝒶ℯ𝒾𝒹 <saeed.rezaee.nor@gmail.com>
2026-08-14 13:05:14 +0330

    feat: add database module

---
+++ database/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from core.core import settings
+
+engine = create_engine(settings.DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = DeclarativeBase()

f3b2fdf5fee90fd407ec25ee26cefa634c9a5717
𝒮𝒶ℯ𝒾𝒹 <saeed.rezaee.nor@gmail.com>
2026-08-18 12:51:51 +0330

    feat: add role to user table

--- database/models/User.py
+++ database/models/User.py
@@ -1,7 +1,15 @@
-from sqlalchemy import String
+from enum import Enum
+
+from sqlalchemy import String, Enum as SqlEnum
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.database import Base
+
+
+class UserRole(str, Enum):
+    SUPERADMIN = "SUPERADMIN"
+    ADMIN = "admin"
+    USER = "user"
 
 
 class User(Base):
@@ -10,3 +18,4 @@ class User(Base):
     email: Mapped[str] = mapped_column(__type_pos=String(255), unique=True, nullable=False)
     username: Mapped[str] = mapped_column(__type_pos=String(50), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(__type_pos=String(255), nullable=False)
+    role: Mapped[UserRole] = mapped_column(__type_pos=SqlEnum(UserRole), nullable=False, default=UserRole.USER)
